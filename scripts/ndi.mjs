f28ab68b66223e0d6ed469251c2645abe1c6b474
Jan Starzak <jan.starzak@gmail.com>
2025-03-17 16:06:38 +0100

    chore: make use of curl on Windows, use global async for downloading

--- scripts/ndi.mjs
+++ scripts/ndi.mjs
@@ -15,30 +15,30 @@
 */
 
 /*  external requirements  */
-const fs        = require("fs")
-const path      = require("path")
-const os        = require("os")
-const shell     = require("shelljs")
-const execa     = require("execa")
-const zip       = require("cross-zip")
-const got       = require("got")
-const mkdirp    = require("mkdirp")
-const tmp       = require("tmp")
-const spawn     = require("cross-spawn")
 
-/*  establish asynchronous environment  */
-;(async () => {
-  /* Check if supported platform and continue only if it is */
-  if (os.platform() === "darwin" || os.platform() === "linux" ||
-      (os.platform() === "win32" && ["ia32", "x64"].includes(os.arch()))) {
+import path from 'node:path'
+import os from 'node:os'
+import shell from 'shelljs'
+import { execa } from 'execa'
+import zip from 'cross-zip'
+import tmp from 'tmp'
+import spawn from 'cross-spawn'
+
+/* Check if supported platform and continue only if it is */
+if (os.platform() === "darwin" || os.platform() === "linux" ||
+    (os.platform() === "win32" && ["ia32", "x64"].includes(os.arch()))) {
     console.log("++ ad-hoc assembling NDK SDK distribution subset from original sources")
     if (os.platform() === "win32") {
         /*  download innoextract utility  */
         const url1 = "https://constexpr.org/innoextract/files/innoextract-1.9-windows.zip"
         console.log("-- downloading innoextract utility")
-        const data1 = await got(url1, { responseType: "buffer" })
+        // const data1 = await got(url1, { responseType: "buffer" })
         const file1 = tmp.tmpNameSync()
-        await fs.promises.writeFile(file1, data1.body, { encoding: null })
+        console.log(file1)        
+        // await fs.promises.writeFile(file1, data1.body, { encoding: null })
+        await execa("curl", ['-L', url1, '-o', file1],
+            { stdin: "inherit", stdout: "inherit", stderr: "inherit" }
+        )
 
         /*  extract innoextract utility  */
         console.log("-- extracting innoextract utility")
@@ -46,17 +46,20 @@ const spawn     = require("cross-spawn")
         zip.unzipSync(file1, dir1)
 
         /*  download NDI SDK distribution  */
-        const url2 = "https://downloads.ndi.tv/SDK/NDI_SDK/NDI 5 SDK.exe"
+        const url2 = "https://downloads.ndi.tv/SDK/NDI_SDK/NDI%205%20SDK.exe"
         console.log("-- dowloading NDI SDK distribution")
-        const data2 = await got(url2, { responseType: "buffer" })
+        // const data2 = await got(url2, { responseType: "buffer" })
         const file2 = tmp.tmpNameSync()
-        await fs.promises.writeFile(file2, data2.body, { encoding: null })
+        // await fs.promises.writeFile(file2, data2.body, { encoding: null })
+        await execa("curl", ['-L', url2, '-o', file2],
+            { stdin: "inherit", stdout: "inherit", stderr: "inherit" }
+        )
 
         /*  extract NDI SDK distribution  */
         console.log("-- extracting NDI SDK distribution")
         const dir2 = tmp.tmpNameSync()
         shell.mkdir("-p", dir2)
-        execa.sync(path.join(dir1, "innoextract.exe"), [ "-s", "-d", dir2, file2 ],
+        await execa(path.join(dir1, "innoextract.exe"), ["-s", "-d", dir2, file2],
             { stdin: "inherit", stdout: "inherit", stderr: "inherit" })
 
         /*  assemble NDI SDK subset  */
@@ -65,7 +68,7 @@ const spawn     = require("cross-spawn")
         shell.mkdir("-p", "ndi/include")
         shell.mkdir("-p", "ndi/lib/win-x86")
         shell.mkdir("-p", "ndi/lib/win-x64")
-        shell.cp(path.join(dir2, "app/Include/*.h"), "ndi/include/")
+        shell.cp("-r", path.join(dir2, "app/Include/"), "ndi")
         shell.cp(path.join(dir2, "app/Lib/x86/Processing.NDI.Lib.x86.lib"), "ndi/lib/win-x86/Processing.NDI.Lib.x86.lib")
         shell.cp(path.join(dir2, "app/Bin/x86/Processing.NDI.Lib.x86.dll"), "ndi/lib/win-x86/Processing.NDI.Lib.x86.dll")
         shell.cp(path.join(dir2, "app/Lib/x64/Processing.NDI.Lib.x64.lib"), "ndi/lib/win-x64/Processing.NDI.Lib.x64.lib")
@@ -82,17 +85,20 @@ const spawn     = require("cross-spawn")
         /*  download NDI SDK distribution  */
         const url1 = "https://downloads.ndi.tv/SDK/NDI_SDK_Mac/Install_NDI_SDK_v5_macOS.pkg"
         console.log("-- dowloading NDI SDK distribution")
-        const data1 = await got(url1, { responseType: "buffer" })
+        // const data1 = await got(url1, { responseType: "buffer" })
         const file1 = tmp.tmpNameSync()
-        await fs.promises.writeFile(file1, data1.body, { encoding: null })
+        // await fs.promises.writeFile(file1, data1.body, { encoding: null })
+        await execa("curl", [url1, '-o', file1],
+            { stdin: "inherit", stdout: "inherit", stderr: "inherit" }
+        )
 
         /*  extract NDI SDK distribution  */
         console.log("-- extracting NDI SDK distribution")
         const dir1 = tmp.tmpNameSync()
         shell.rm("-rf", dir1)
-        execa.sync("pkgutil", [ "--expand", file1, dir1 ],
+        await execa("pkgutil", ["--expand", file1, dir1],
             { stdin: "inherit", stdout: "inherit", stderr: "inherit" })
-        execa.sync("cpio", [ "-idmu", "-F", path.join(dir1, "NDI_SDK_Component.pkg/Payload") ],
+        await execa("cpio", ["-idmu", "-F", path.join(dir1, "NDI_SDK_Component.pkg/Payload")],
             { cwd: dir1, stdin: "inherit", stdout: "ignore", stderr: "ignore" })
 
         /*  assemble NDI SDK subset  */
@@ -113,17 +119,20 @@ const spawn     = require("cross-spawn")
         /*  download NDI SDK distribution  */
         const url1 = "https://downloads.ndi.tv/SDK/NDI_SDK_Linux/Install_NDI_SDK_v5_Linux.tar.gz"
         console.log("-- dowloading NDI SDK distribution")
-        const data1 = await got(url1, { responseType: "buffer" })
+        // const data1 = await got(url1, { responseType: "buffer" })
         const file1 = tmp.tmpNameSync()
-        await fs.promises.writeFile(file1, data1.body, { encoding: null })
+        // await fs.promises.writeFile(file1, data1.body, { encoding: null })
+        await execa("curl", [url1, '-o', file1],
+            { stdin: "inherit", stdout: "inherit", stderr: "inherit" }
+        )
 
         /*  extract NDI SDK distribution  */
         console.log("-- extracting NDI SDK distribution")
         const dir1 = tmp.tmpNameSync()
         shell.mkdir("-p", dir1)
-        execa.sync("tar", [ "-z", "-x", "-C", dir1, "-f", file1 ],
+        await execa("tar", ["-z", "-x", "-C", dir1, "-f", file1],
             { stdin: "inherit", stdout: "inherit", stderr: "inherit" })
-        execa.sync("sh", [ "-c", `echo "y" | PAGER=cat sh Install_NDI_SDK_v5_Linux.sh` ],
+        await execa("sh", ["-c", `echo "y" | PAGER=cat sh Install_NDI_SDK_v5_Linux.sh`],
             { cwd: dir1, stdin: "inherit", stdout: "ignore", stderr: "inherit" })
 
         /*  assemble NDI SDK subset  */
@@ -135,7 +144,7 @@ const spawn     = require("cross-spawn")
         shell.mkdir("-p", "ndi/lib/lnx-armv7l")
         shell.mkdir("-p", "ndi/lib/lnx-arm64")
         shell.mv(path.join(dir1, "NDI SDK for Linux/include/*.h"), "ndi/include/")
-        shell.mv(path.join(dir1, "NDI SDK for Linux/lib/i686-linux-gnu/*"),   "ndi/lib/lnx-x86/")
+        shell.mv(path.join(dir1, "NDI SDK for Linux/lib/i686-linux-gnu/*"), "ndi/lib/lnx-x86/")
         shell.mv(path.join(dir1, "NDI SDK for Linux/lib/x86_64-linux-gnu/*"), "ndi/lib/lnx-x64/")
         shell.mv(path.join(dir1, "NDI SDK for Linux/lib/arm-rpi4-linux-gnueabihf/*"), "ndi/lib/lnx-armv7l/")
         shell.mv(path.join(dir1, "NDI SDK for Linux/lib/aarch64-rpi4-linux-gnueabi/*"), "ndi/lib/lnx-arm64/")
@@ -151,8 +160,4 @@ const spawn     = require("cross-spawn")
         input: "Native module build required.",
         stdio: "inherit"
     })
-  }
-})().catch((err) => {
-    console.log(`** ERROR: ${err}`)
-})
-
+}
